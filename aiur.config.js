f2fa1357bbea1f6e0fa97a929b29c36135be2ae3
Lucas Dohmen <lucas@dohmen.io>
2020-02-20 01:55:38 +0100

    Switch to preview of aiur 0.6, fixing watch mode

--- aiur.config.js
+++ aiur.config.js
@@ -1,16 +1,34 @@
-exports.sass = [{
-  source: "./app/assets/stylesheets/application.scss"
-}, {
-  source: "./app/assets/stylesheets/styleguide.scss"
-}];
+module.exports = {
+  title: "Example",
+  language: "en",
+  description: "An example for our workshop",
 
-exports.pages = {
-  "": "./app/components/README.md",
-  colors: "./app/components/colors.md",
-  flash: "./app/components/flash/README.md",
-  navbar: "./app/components/navbar/README.md"
+  pages: {
+    "": "./app/components/README.md",
+    colors: "./app/components/colors.md",
+    flash: "./app/components/flash/README.md",
+    navbar: "./app/components/navbar/README.md"
+  },
+
+  snippetAssets: {
+    sass: [
+      {
+        source: "./app/assets/stylesheets/application.scss",
+        target: "./application.css"
+      },
+      {
+        source: "./app/assets/stylesheets/styleguide.scss",
+        target: "./styleguide.css"
+      }
+    ],
+
+    static: [
+      {
+        source: "./app/assets/images",
+        target: "./images"
+      }
+    ]
+  },
+
+  watchDirs: ["./app/assets", "./app/components"]
 };
-
-exports.title = "Example";
-exports.language = "en";
-exports.description = "An example for our workshop";
