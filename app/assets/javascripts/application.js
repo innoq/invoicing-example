6912a617d6e38b6dae470e7da741ef345674cada
Joy Clark <joy.clark@innoq.com>
2020-03-03 10:03:34 +0100

    Add support for non-subgrid, fix columsn for tabelle-header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
-console.log("Hello World");
+// We need Customized built-in elements from Safari
+import 'document-register-element'
 
 import "../../components/tabelle/tabelle";
 import "../../components/tabelle-toggler/tabelle-toggler";
